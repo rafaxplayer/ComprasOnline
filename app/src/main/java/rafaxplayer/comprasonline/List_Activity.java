c507be5653e43a013c49c1fa24238246cfd0ac85
rafaxplayer <rafaxplayer@hotmail.com>
2016-07-04 09:50:04 +0200

    salvado estado de la lista al resumir

--- app/src/main/java/rafaxplayer/comprasonline/List_Activity.java
+++ app/src/main/java/rafaxplayer/comprasonline/List_Activity.java
@@ -6,20 +6,25 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 import java.util.ArrayList;
 import rafaxplayer.comprasonline.CLASS.ListPlaces_Adapter;
 import rafaxplayer.comprasonline.MODELS.Place;
 public class List_Activity extends AppCompatActivity  {
     private Toolbar toolbar;
     private RecyclerView list;
-
+    public static int index = -1;
+    public static int top = -1;
+    private LinearLayoutManager lngManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        lngManager=new LinearLayoutManager(getApplicationContext());
         list = (RecyclerView) findViewById(R.id.list);
-        list.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        list.setLayoutManager(lngManager);
         list.setItemAnimator(new DefaultItemAnimator());
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -37,6 +42,22 @@ public class List_Activity extends AppCompatActivity  {
         if (places != null) {
             list.setAdapter(new ListPlaces_Adapter(this,List_Activity.this, places));
         }
+        //rstore list position
+        if(index != -1)
+        {
+            lngManager.scrollToPositionWithOffset( index, top);
+        }
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        //read current recyclerview position
+        index = lngManager.findFirstVisibleItemPosition();
+        View v = list.getChildAt(0);
+        top = (v == null) ? 0 : (v.getTop() - list.getPaddingTop());
     }
 
 
